4ea35399bad4139f105b09c93393e3e87941d0fd
Mattia Maucioni <mattiamaucioni@icloud.com>
2024-04-19 10:04:44 +0200

    Introdotto styling sistema login

--- src/main/java/com/paspa/InterfacciaApp.java
+++ src/main/java/com/paspa/InterfacciaApp.java
@@ -1,6 +1,10 @@
 package com.paspa;
 
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,6 +16,7 @@ import java.sql.SQLException;
 public class InterfacciaApp {
 
     private String username;
+    private JPanel contentPane;
     private JTextField usernameField;
     private JPasswordField passwordField;
 
@@ -23,30 +28,47 @@ public class InterfacciaApp {
             e.printStackTrace();
         }
 
-        JFrame frame = new JFrame("Inserisci credenziali SnipeIT");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = (int) screenSize.getWidth();
+        int screenHeight = (int) screenSize.getHeight();
 
-        frame.setSize(500, 300);
+        // Calcola le coordinate x e y per centrare la finestra
+        int windowWidth = 450; // larghezza della finestra
+        int windowHeight = 350; // altezza della finestra
+        int x = (screenWidth - windowWidth) / 2;
+        int y = (screenHeight - windowHeight) / 2;
+
+		JFrame frame = new JFrame("Inserisci credenziali SnipeIT");
+		
+		frame.setResizable(false);
+		frame.setSize(500, 300);
         frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBackground(Color.decode("#333333"));
-
-        ImageIcon icon = new ImageIcon(getClass().getResource("/img/exe.png"));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(x, y, windowWidth, windowHeight);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.decode("#333333"));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		ImageIcon icon = new ImageIcon(getClass().getResource("/img/exe.png"));
         frame.setIconImage(icon.getImage()); // Imposta l'icona della finestra
 
-        JPanel loginPanel = new JPanel();
-        loginPanel.setLayout(new GridLayout(3, 1));
-        
-        JLabel usernameLabel = new JLabel("Username:");
-        usernameField = new JTextField();
-        
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordField = new JPasswordField();
-        
-        JButton buttonLogin = new JButton("Login");
-        buttonLogin.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+		frame.setContentPane(contentPane);
+		
+		JPanel loginPanel = new JPanel();
+		loginPanel.setBackground(Color.decode("#333333"));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(loginPanel, GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(loginPanel, GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE)
+		);
+		
+		JButton buttonLogin = new JButton("Login");
+		buttonLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
                 username = usernameField.getText();
                 String password = new String(passwordField.getPassword());
 
@@ -68,17 +90,74 @@ public class InterfacciaApp {
 
                     MainFrame();
                 }
-            }
-        });
-        
-        loginPanel.add(usernameLabel);
-        loginPanel.add(usernameField);
-        loginPanel.add(passwordLabel);
-        loginPanel.add(passwordField);
-        loginPanel.add(buttonLogin);
-        
-        frame.add(loginPanel);
-        
+			}
+		});
+		
+		JLabel usernameLabel = new JLabel("USERNAME");
+		usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		usernameLabel.setFont(new Font("Montserrat Black", Font.BOLD, 11));
+		usernameLabel.setForeground(new Color(255, 255, 255));
+		
+		usernameField = new JTextField();
+		usernameField.setFont(new Font("Arial", Font.PLAIN, 12));
+		usernameField.setColumns(10);
+		
+		JLabel passwordLabel = new JLabel("PASSWORD");
+		passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordLabel.setForeground(Color.WHITE);
+		passwordLabel.setFont(new Font("Montserrat Black", Font.BOLD, 11));
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Arial", Font.PLAIN, 12));
+		passwordField.setColumns(10);
+		
+		JLabel logoPagano = new JLabel("");
+		logoPagano.setIcon(new ImageIcon("src\\main\\resources\\img\\logo-pagano.png"));
+		GroupLayout gl_loginPanel = new GroupLayout(loginPanel);
+		gl_loginPanel.setHorizontalGroup(
+			gl_loginPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_loginPanel.createSequentialGroup()
+					.addGap(87)
+					.addComponent(logoPagano)
+					.addContainerGap(87, Short.MAX_VALUE))
+				.addGroup(gl_loginPanel.createSequentialGroup()
+					.addGap(168)
+					.addComponent(buttonLogin, GroupLayout.DEFAULT_SIZE, 89, Short.MAX_VALUE)
+					.addGap(167))
+				.addGroup(gl_loginPanel.createSequentialGroup()
+					.addGap(166)
+					.addComponent(usernameLabel, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(166, Short.MAX_VALUE))
+				.addGroup(gl_loginPanel.createSequentialGroup()
+					.addGap(171)
+					.addComponent(passwordLabel, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(171, Short.MAX_VALUE))
+				.addGroup(gl_loginPanel.createSequentialGroup()
+					.addGap(107)
+					.addGroup(gl_loginPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 210, GroupLayout.PREFERRED_SIZE)
+						.addComponent(usernameField, GroupLayout.PREFERRED_SIZE, 210, GroupLayout.PREFERRED_SIZE))
+					.addGap(107))
+		);
+		gl_loginPanel.setVerticalGroup(
+			gl_loginPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_loginPanel.createSequentialGroup()
+					.addGap(26)
+					.addComponent(logoPagano)
+					.addGap(1)
+					.addComponent(usernameLabel, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(usernameField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(passwordLabel, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+					.addGap(4)
+					.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(buttonLogin)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		loginPanel.setLayout(gl_loginPanel);
+		contentPane.setLayout(gl_contentPane);
         frame.setVisible(true);
     }
 
